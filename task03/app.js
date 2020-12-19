087e90d07952043f8e1f266dfbcd5f0df6d6dac3
unknown <vladodimovski99@gmail.com>
2020-12-19 20:53:41 +0100

    fix

--- task03/app.js
+++ task03/app.js
@@ -1,12 +1,6 @@
-const express = require('express');
-const app = express();
-const path = require('path');
-const port = 3000;
-
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname + '/index.html'));
-});
-
-app.listen(port, () => console.log(`url-shortener listening on port ${port}!`));
-
-
+var http = require('http');
+http.createServer(function (req,res){
+    res.writeHead(200,{'Content-type' : 'text/html'});
+    res.write('index.html');
+    res.end();
+}).listen(8080);
