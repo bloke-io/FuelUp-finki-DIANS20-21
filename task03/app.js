cbf05f1ae40517d451779da705a0d2e48ea57bd1
bloke-io <66572941+bloke-io@users.noreply.github.com>
2020-12-19 22:38:56 +0100

    Update app.js

--- task03/app.js
+++ task03/app.js
@@ -1,23 +1,18 @@
-var express = require('express');
-var app = express();
+const http = require('http')
+const fs = require('fs')
+const port = 8080
 
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
-var port = process.env.PORT || 8080;
+const server = http.createServer(function (req, res){
+    res.writeHead(200, {'Content-type' : 'text/html'})
+    fs.readFile('index.html', function (error, data){
+        if(error){
+            res.writeHead(404)
+            res.write('Error: file not found')
+        }else {
+            res.write(data)
+        }
+        res.end()
+    })
+})
 
-// set the view engine to ejs
-app.set('view engine', 'ejs');
-
-// make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/public'));
-
-// set the home page route
-app.get('/', function(req, res) {
-
-    // ejs render automatically looks in the views folder
-    res.render('index');
-});
-
-app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
-});
+server.listen(port)
