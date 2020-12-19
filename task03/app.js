d2217d24223533715d2f910e7f1d486658ae795a
unknown <vladodimovski99@gmail.com>
2020-12-19 22:00:01 +0100

    fix v1

--- task03/app.js
+++ task03/app.js
@@ -1,38 +1,13 @@
-const http = require('http');
-<<<<<<< HEAD
-const fs = require('fs');
-const port = 8080;
+const http = require('http')
+const fs = require('fs')
+const port = 8080
 
 const server = http.createServer(function (req, res){
     res.writeHead(200, {'Content-type' : 'text/html'})
-    fs.readFile('index.html', function (error, data){
-        if(error){
-            res.writeHead(404)
-            res.write('Error: file not found')
-        }else {
-            res.write(data)
-        }
+    fs.readFile('index.html', function(data){
+        res.write(data)
         res.end()
     })
-
 })
 
-server.listen(port, function (error){
-    if(error){
-        console.log('Something went wrong', error)
-    }else{
-        console.log('Server is listening on port' + port)
-    }
-})
-=======
-const fs = require('fs').promises;
-const server = http.createServer(function (req, res){
-    fs.readFile(__dirname + "/index.html")
-        .then(contents => {
-            res.setHeader("Content-Type", "text/html");
-            res.writeHead(200);
-            res.end(contents);
-        })
-});
-
->>>>>>> 60c8435c517762a81bb0f6490d0be00f05294fbb
+server.listen(port)
