0f686702192de9ceada87f76e6d5acac3f40c9fa
unknown <vladodimovski99@gmail.com>
2020-12-19 22:05:38 +0100

    fix v2

--- task03/app.js
+++ task03/app.js
@@ -4,8 +4,13 @@ const port = 8080
 
 const server = http.createServer(function (req, res){
     res.writeHead(200, {'Content-type' : 'text/html'})
-    fs.readFile('index.html', function(data){
-        res.write(data)
+    fs.readFile('index.html', function (error, data){
+        if(error){
+            res.writeHead(404)
+            res.write('Error: file not found')
+        }else {
+            res.write(data)
+        }
         res.end()
     })
 })
