48c2f860319334ba50a968cf3744f5aaa5ebfa98
GjorgeA <gjorge.argirov@outlook.com>
2021-01-26 22:18:32 +0100

    updatev3.0

--- task03/public/javascript/javascript.js
+++ task03/public/javascript/javascript.js
@@ -297,20 +297,13 @@ function NearestFuel(latitude, longitude) {
 }
 
 function getLocation() {
-    var successHandler = function(position) {
-        alert(position.coords.latitude);
-        alert(position.coords.longitude);
-    };
-
-    var errorHandler = function (errorObj) {
-        alert(errorObj.code + ": " + errorObj.message);
-
-        alert("something wrong take this lat " + 26.0546106 );
-        alert("something wrong take this lng " +-98.3939791);
-
-    };
-
-    navigator.geolocation.getCurrentPosition( successHandler, errorHandler, {enableHighAccuracy: true, maximumAge: 10000});
+    navigator.geolocation.getCurrentPosition(success => {
+        alert('Ja zede be');
+    }, failure => {
+        if (failure.message.startsWith("Only secure origins are allowed")) {
+            alert('nema air od ova');
+        }
+    });
 }
 
 
