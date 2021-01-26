221958ee1cd9663f271cb616634f684dccbe914f
GjorgeA <gjorge.argirov@outlook.com>
2021-01-26 22:11:14 +0100

    updatev3.0

--- task03/public/javascript/javascript.js
+++ task03/public/javascript/javascript.js
@@ -297,14 +297,20 @@ function NearestFuel(latitude, longitude) {
 }
 
 function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-        alert( "Geolocation is not supported by this browser.");
-    } else {
-        alert( "Geolocation is not supported by this browser.");
-    }
+    var successHandler = function(position) {
+        alert(position.coords.latitude);
+        alert(position.coords.longitude);
+    };
+
+    var errorHandler = function (errorObj) {
+        alert(errorObj.code + ": " + errorObj.message);
+
+        alert("something wrong take this lat " + 26.0546106 );
+        alert("something wrong take this lng " +-98.3939791);
+
+    };
+
+    navigator.geolocation.getCurrentPosition( successHandler, errorHandler, {enableHighAccuracy: true, maximumAge: 10000});
 }
 
-function showPosition(position) {
-    alert( "Latitude: " + position.coords.latitude +"<br>Longitude: " + position.coords.longitude);
-}
+
